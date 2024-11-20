df3dca36b48784c4a9bb5faf4535d031c76c35b3
ymd65536 <ymd65536@gmail.com>
2024-11-20 21:21:01 +0900

    add: subnet

--- Program.cs
+++ Program.cs
@@ -1,30 +1,58 @@
-﻿using Azure;
+﻿using System;
+using Azure;
+using Azure.Core;
 using Azure.Identity;
 using Azure.ResourceManager;
+using Azure.ResourceManager.Compute;
+using Azure.ResourceManager.Network;
 using Azure.ResourceManager.Resources;
 
 var credential = new DefaultAzureCredential(
-  new DefaultAzureCredentialOptions()
-  {
-    ExcludeVisualStudioCredential = false,
-  }
+    new DefaultAzureCredentialOptions()
+    {
+        ExcludeVisualStudioCredential = false,
+    }
 );
-var armClient = new ArmClient(credential);
-var subscription = await armClient.GetDefaultSubscriptionAsync();
 
 // リソースグループの作成
-string resourceGroupName = "MyResourceGroup";
-string location = "eastus";
+string resourceGroupName = "myResourceGroup";
+var armClient = new ArmClient(credential);
 
-try
+SubscriptionResource subscription = armClient.GetDefaultSubscription();
+bool exist = await subscription.GetResourceGroups().ExistsAsync(resourceGroupName);
+
+if (!exist)
 {
-	ResourceGroupData resourceGroupData = new ResourceGroupData(location);
-	ArmOperation<ResourceGroupResource> operation = await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, resourceGroupName, resourceGroupData);
-	ResourceGroupResource resourceGroup = operation.Value;
-	Console.WriteLine($"Resource Group '{resourceGroup.Data.Name}' created in location '{resourceGroup.Data.Location}'.");  
+    Console.WriteLine("ResourceGroup does not exist.");
+    await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, resourceGroupName, new ResourceGroupData(AzureLocation.EastUS));
+    Console.WriteLine("ResourceGroup created.");
+}else{
+    Console.WriteLine("ResourceGroup already exists.");
+    ResourceGroupResource resourceGroup = subscription.GetResourceGroup(resourceGroupName);
+    await resourceGroup.DeleteAsync(WaitUntil.Completed);
+    Console.WriteLine("ResourceGroup deleted.");
+    await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, resourceGroupName, new ResourceGroupData(AzureLocation.EastUS));
+    Console.WriteLine("ResourceGroup created.");
 }
-catch (System.Exception)
+
+// ネットワークの作成
+
+string vnetName = "myVnet";
+
+VirtualNetworkData vnetData = new VirtualNetworkData()
 {
-  
-  throw;
-}
+    Location = AzureLocation.EastUS,
+    AddressPrefixes = {"10.0.0.0/16"}
+};
+
+ResourceGroupResource Rg = await subscription.GetResourceGroups().GetAsync(resourceGroupName);
+await Rg.GetVirtualNetworks().CreateOrUpdateAsync(WaitUntil.Completed, vnetName, vnetData);
+
+string subnetName = $"{vnetName}mySubnet";
+SubnetData subnetData = new SubnetData()
+{
+    Name = subnetName,
+    AddressPrefix = "10.0.1.0/24",
+};
+
+await Rg.GetVirtualNetworks().Get(vnetName).Value.GetSubnets().CreateOrUpdateAsync(WaitUntil.Completed, subnetName, subnetData);
