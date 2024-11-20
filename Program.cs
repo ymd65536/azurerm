59122f2b56c52a8bf0430a38a6a920964f8411f5
ymd65536 <ymd65536@gmail.com>
2024-11-20 14:07:07 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Azure;
+using Azure.Identity;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
+
+var credential = new DefaultAzureCredential(
+  new DefaultAzureCredentialOptions()
+  {
+    ExcludeVisualStudioCredential = false,
+  }
+);
+var armClient = new ArmClient(credential);
+var subscription = await armClient.GetDefaultSubscriptionAsync();
+
+// リソースグループの作成
+string resourceGroupName = "MyResourceGroup";
+string location = "eastus";
+
+ResourceGroupData resourceGroupData = new ResourceGroupData(location);
+ArmOperation<ResourceGroupResource> operation = await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, resourceGroupName, resourceGroupData);
+ResourceGroupResource resourceGroup = operation.Value;
+
+Console.WriteLine($"Resource Group '{resourceGroup.Data.Name}' created in location '{resourceGroup.Data.Location}'.");
